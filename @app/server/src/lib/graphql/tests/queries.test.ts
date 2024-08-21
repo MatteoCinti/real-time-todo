ef6b20c53b5324e91d544488b877d8d842260fac
MatteoCinti <matteo.cinti@barnebys.com>
2024-08-21 16:58:34 +0200

    upadted user in build script

--- @app/server/src/lib/graphql/tests/queries.test.ts
+++ @app/server/src/lib/graphql/tests/queries.test.ts
@@ -6,29 +6,6 @@ describe('Apollo Server', () => {
     expect(testServer).toBeDefined();
   });
 
-  describe('getTodos ', () => {
-    it('should return a list of todos', async () => {
-      const response = (await testServer.executeOperation({
-        query: `#graphql 
-            query GetTodos { 
-                getTodos { title description id isDone board}
-            }`
-      })) as any;
-
-      expect(response.body.kind).toBe('single');
-      expect(response.body.singleResult.data.getTodos.length).toBeGreaterThan(
-        1
-      );
-      expect(response.body.singleResult.data.getTodos[0]).toMatchObject({
-        id: mocks.Int(),
-        board: mocks.Int(),
-        title: mocks.String(),
-        isDone: mocks.Boolean(),
-        description: mocks.String()
-      });
-    });
-  });
-
   describe('login', () => {
     it('should return a user', async () => {
       const response = (await testServer.executeOperation({
