9cc803b050b71adb901c3d9b11be4fd3c812bd90
MatteoCinti <matteo.cinti@barnebys.com>
2024-08-24 12:00:25 +0200

    improved page layout

--- @app/client/src/pages/home/home.tsx
+++ @app/client/src/pages/home/home.tsx
@@ -4,12 +4,12 @@ import { Card } from '~/components/ui';
 
 function Home() {
   return (
-    <Card className="border-muted flex h-full w-full rounded-t-none p-0">
+    <Card className="border-muted grid h-full w-full grid-cols-8 gap-4 rounded-t-none p-0">
       {/* <CardHeader>Home</CardHeader> */}
-      <Card className="border-muted m-4 h-full w-1/3">
+      <Card className="border-muted col-span-2 my-4 ml-4">
         <BoardList />
       </Card>
-      <Card className="h-1/2 w-1/2">
+      <Card className="border-muted col-span-6 my-4 mr-4">
         <Outlet />
       </Card>
     </Card>
